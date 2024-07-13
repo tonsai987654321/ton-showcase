57cc0210fbc7724f15c30ebd3cd870a4e9282346
tonsai987654321 <75606675+tonsai987654321@users.noreply.github.com>
2024-07-13 11:45:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 # st.title('😎Ton data showcase')
 st.markdown("<h1 style='text-align: center; color: #BD98E3;'>😎Ton data showcase</h1>", unsafe_allow_html=True)
 
+
+
 st.write('My first time with Streamlit🐔')
